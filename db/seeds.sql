605c92f12506c4a25146dbf8a93dde40826f1205
skanakia <skanakia1@gmail.com>
2018-06-28 00:49:53 -0400

    first

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (burger_name, devoured) VALUES ("Bacon Cheese Burger", false);
+INSERT INTO burger (burger_name, devoured) VALUES ("Veggie Burger", false);
+INSERT INTO burger (burger_name, devoured) VALUES ("Red Hot and Bleu Burger", false);
